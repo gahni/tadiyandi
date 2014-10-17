bbad888ef335477dc0b749de6750f9cd71057edb
torrentholm600@gmail.com <torrentholm600@gmail.com@ad8d5aa7-1892-c525-523f-536220f915de>
2014-10-17 03:18:27 +0000

    fenrir essentials, no conf yet for 99/70

---
+++ fenrir/npc/custom/pony/del.sql
@@ -0,0 +1,21 @@
+delete from inventory where nameid=4407;
+delete from inventory where nameid=4399;
+delete from inventory where nameid=4376;
+delete from inventory where nameid=4305;
+delete from inventory where nameid=4318;
+delete from inventory where nameid=4236;
+
+
+delete from storage where nameid=4407;
+delete from storage where nameid=4399;
+delete from storage where nameid=4376;
+delete from storage where nameid=4305;
+delete from storage where nameid=4318;
+delete from storage where nameid=4236;
+
+delete from cart_inventory where nameid=4407;
+delete from cart_inventory where nameid=4399;
+delete from cart_inventory where nameid=4376;
+delete from cart_inventory where nameid=4305;
+delete from cart_inventory where nameid=4318;
+delete from cart_inventory where nameid=4236;
